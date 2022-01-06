0b205aa1ad21dabe3ce6d065bc6caab52978d9ef
Becca <beccasullivan30@gmail.com>
2022-01-06 14:52:02 +0000

    render list of categories

--- marketplace-app/src/components/CategoriesList.jsx
+++ marketplace-app/src/components/CategoriesList.jsx
@@ -1,14 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getCategories } from "../utils/api";
 
 const CategoriesList = () => {
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
     getCategories().then((categoriesFromApi) => {
-      console.log(categoriesFromApi);
+      setCategories(categoriesFromApi);
     });
   }, []);
 
-  return null;
+  return (
+    <main>
+      <h1>Categories</h1>
+      <ul>
+        {categories.map((category) => {
+          return <li key={category.category_name}>{category.category_name}</li>;
+        })}
+      </ul>
+    </main>
+  );
 };
 
 export default CategoriesList;
