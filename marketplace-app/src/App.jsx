ea510705012588844e773f1cfe62434fc14cd32d
Becca <beccasullivan30@gmail.com>
2022-01-06 16:22:22 +0000

    add route to single category and move items state

--- marketplace-app/src/App.jsx
+++ marketplace-app/src/App.jsx
@@ -1,13 +1,17 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { useState } from "react";
 import Home from "./components/Home";
 import CategoriesList from "./components/CategoriesList";
 import NavBar from "./components/NavBar";
 import ItemsList from "./components/ItemsList";
 import SingleItem from "./components/SingleItem";
+import SingleCategory from "./components/SingleCategory";
 
 
 function App() {
+  const [items, setItems] = useState([]);
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -15,8 +19,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<CategoriesList />}></Route>
-          <Route path="/items" element={<ItemsList />}></Route>
+          <Route path="/items" element={<ItemsList items={items} setItems={setItems}/>}></Route>
           <Route path="/items/:item_id" element={<SingleItem />}></Route>
+          <Route path="/items/:item_id" element={<SingleItem />}></Route>
+          <Route path="/categories/:category_name" element={<SingleCategory items={items} setItems={setItems}/>}></Route>
         </Routes>
       </div>
     </BrowserRouter>
