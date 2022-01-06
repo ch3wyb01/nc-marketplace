1142439a1211e4bf2d9b26664008c08e5589e9a7
Becca <beccasullivan30@gmail.com>
2022-01-06 15:31:10 +0000

    add route to single item

--- marketplace-app/src/App.jsx
+++ marketplace-app/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/Home";
 import CategoriesList from "./components/CategoriesList";
 import NavBar from "./components/NavBar";
 import ItemsList from "./components/ItemsList";
+import SingleItem from "./components/SingleItem";
+
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<CategoriesList />}></Route>
           <Route path="/items" element={<ItemsList />}></Route>
+          <Route path="/items/:item_id" element={<SingleItem />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
