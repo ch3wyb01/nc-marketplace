439e9b0fee574ad1128e94f594ff997709552333
Becca <beccasullivan30@gmail.com>
2022-01-06 14:56:58 +0000

    add NavBar

--- marketplace-app/src/App.jsx
+++ marketplace-app/src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import CategoriesList from "./components/CategoriesList";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/categories" element={<CategoriesList />}></Route>
