9627eb2c0263bb2b3571947cd8696b93ef8b0e10
Becca <beccasullivan30@gmail.com>
2022-01-07 15:39:11 +0000

    add success message and sell another item button after submitting form

--- marketplace-app/src/components/SellItem.jsx
+++ marketplace-app/src/components/SellItem.jsx
@@ -1,6 +1,12 @@
+import { useState } from "react";
 import { postItem } from "../utils/api";
+import { useNavigate } from "react-router-dom";
 
 const SellItem = ({ setItems }) => {
+  const [isListed, setIsListed] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleSubmit = (event) => {
     event.preventDefault();
     const newItem = event.target;
@@ -12,33 +18,51 @@ const SellItem = ({ setItems }) => {
       newItem[4].value
     ).then((listedItem) => {
       setItems((currItems) => [...currItems, listedItem]);
+      setIsListed(true);
     });
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="name">
-        Item name:
-        <input type="text" id="name" name="name" required />
-      </label>
-      <label htmlFor="img_url">
-        Image URL:
-        <input type="text" id="img_url" name="img_url" required />
-      </label>
-      <label htmlFor="price">
-        Price in pence:
-        <input type="text" id="price" name="price" required />
-      </label>
-      <label htmlFor="description">
-        Description:
-        <input type="text" id="description" name="description" />
-      </label>
-      <label htmlFor="category_name">
-        Category:
-        <input type="text" id="category_name" name="category_name" />
-      </label>
-      <button>List item</button>
-    </form>
+    <main>
+      {isListed ? (
+        <section>
+          <p>Item listed!</p>
+
+          <button
+            onClick={() => {
+              setIsListed(false);
+              navigate("/items/sell");
+            }}
+          >
+            List another item for sale
+          </button>
+        </section>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">
+            Item name:
+            <input type="text" id="name" name="name" required />
+          </label>
+          <label htmlFor="img_url">
+            Image URL:
+            <input type="text" id="img_url" name="img_url" required />
+          </label>
+          <label htmlFor="price">
+            Price in £:
+            <input type="text" id="price" name="price" required />
+          </label>
+          <label htmlFor="description">
+            Description:
+            <input type="text" id="description" name="description" />
+          </label>
+          <label htmlFor="category_name">
+            Category:
+            <input type="text" id="category_name" name="category_name" />
+          </label>
+          <button>List item</button>
+        </form>
+      )}
+    </main>
   );
 };
 
