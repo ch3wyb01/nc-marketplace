8c2db3cae94aa448a2b00b76cf17ed257b1cf85b
Becca <beccasullivan30@gmail.com>
2022-01-07 14:38:36 +0000

    basic list new item functionality

--- marketplace-app/src/components/SellItem.jsx
+++ marketplace-app/src/components/SellItem.jsx
@@ -1,25 +1,41 @@
+import { postItem } from "../utils/api";
+
 const SellItem = ({ setItems }) => {
   const handleSubmit = (event) => {
-      event.preventDefault();
-    console.log(event);
+    event.preventDefault();
+    const newItem = event.target;
+    postItem(
+      newItem[0].value,
+      newItem[1].value,
+      newItem[2].value,
+      newItem[3].value,
+      newItem[4].value
+    ).then((listedItem) => {
+      setItems((currItems) => [...currItems, listedItem]);
+    });
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <label htmlFor="name">Item name:
-      <input type="text" id="name" name="name" required />
+      <label htmlFor="name">
+        Item name:
+        <input type="text" id="name" name="name" required />
       </label>
-      <label htmlFor="img_url">Image URL:
-      <input type="text" id="img_url" name="img_url" required />
+      <label htmlFor="img_url">
+        Image URL:
+        <input type="text" id="img_url" name="img_url" required />
       </label>
-      <label htmlFor="price">Price in pence:
-      <input type="text" id="price" name="price" required />
+      <label htmlFor="price">
+        Price in pence:
+        <input type="text" id="price" name="price" required />
       </label>
-      <label htmlFor="description">Description:
-      <input type="text" id="description" name="description" />
+      <label htmlFor="description">
+        Description:
+        <input type="text" id="description" name="description" />
       </label>
-      <label htmlFor="category_name">Category:
-      <input type="text" id="category_name" name="category_name" />
+      <label htmlFor="category_name">
+        Category:
+        <input type="text" id="category_name" name="category_name" />
       </label>
       <button>List item</button>
     </form>
