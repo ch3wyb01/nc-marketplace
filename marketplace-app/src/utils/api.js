20951b0afe58b877673543ad182c775c86399014
Becca <beccasullivan30@gmail.com>
2022-01-07 12:57:46 +0000

    create getItemsByCategory

--- marketplace-app/src/utils/api.js
+++ marketplace-app/src/utils/api.js
@@ -16,8 +16,18 @@ export const getItems = () => {
   });
 };
 
+export const getItemsByCategory = (category) => {
+  return marketplaceApi.get(`/items?category_name=${category}`).then((res) => {
+    return res.data.items;
+  });
+};
+
 export const getItem = (item_id) => {
   return marketplaceApi.get(`/items/${item_id}`).then((res) => {
     return res.data.item;
   });
 };
+
+// export const postItem = (item_name, img_url, price, description, category_name) => {
+//   return marketplaceApi.post('/items', {item_name, img_url, price, description, category_name})
+// }
