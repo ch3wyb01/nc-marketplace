84218145d491d1cb596bdc41858961b30465eb04
Becca <beccasullivan30@gmail.com>
2022-01-07 12:50:04 +0000

    add submit

--- marketplace-app/src/components/SellItem.jsx
+++ marketplace-app/src/components/SellItem.jsx
@@ -1,16 +1,26 @@
 const SellItem = ({ setItems }) => {
+  const handleSubmit = (event) => {
+      event.preventDefault();
+    console.log(event);
+  };
+
   return (
-    <form action="submit">
-      <label for="name">Item name:</label>
-      <input type="text" id="name" name="name" required /><br />
-      <label for="img_url">Image URL:</label>
-      <input type="text" id="img_url" name="img_url" required /><br />
-      <label for="price">Price in pence:</label>
-      <input type="text" id="price" name="price" required /><br />
-      <label for="description">Description:</label>
-      <input type="text" id="description" name="description" /><br />
-      <label for="category_name">Category:</label>
-      <input type="text" id="category_name" name="category_name" /><br />
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name">Item name:
+      <input type="text" id="name" name="name" required />
+      </label>
+      <label htmlFor="img_url">Image URL:
+      <input type="text" id="img_url" name="img_url" required />
+      </label>
+      <label htmlFor="price">Price in pence:
+      <input type="text" id="price" name="price" required />
+      </label>
+      <label htmlFor="description">Description:
+      <input type="text" id="description" name="description" />
+      </label>
+      <label htmlFor="category_name">Category:
+      <input type="text" id="category_name" name="category_name" />
+      </label>
       <button>List item</button>
     </form>
   );
